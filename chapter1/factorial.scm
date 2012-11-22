6db471116482cb1fa1d1f1b0e807bd97a3568aed
Manu S Ajith <neo@codingarena.in>
2012-11-23 01:53:51 +0530

    Factorial of no

---
+++ chapter1/factorial.scm
@@ -0,0 +1,15 @@
+(define (factorial n)
+	(if (= n 1)
+				1
+			(* n (factorial (- n 1)))))
+
+;another method for factorial
+(define (factorial n)
+	(fact-iter 1 1 n))
+
+(define (fact-iter product counter max-count)
+	(if (> counter max-count)
+				product
+			(fact-iter (* counter product)
+	(+ counter 1)
+	max-count)))
